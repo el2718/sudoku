fa846dc97c08eb1ba86cd2ab6399ae215388551b
el2718 <45861675+el2718@users.noreply.github.com>
2023-06-02 10:49:28 +0800

    Update sudoku.f90

--- sudoku.f90
+++ sudoku.f90
@@ -3,9 +3,10 @@
 
 program main
 implicit none
-integer::i, j, k, sudokus(9,9,5)
+integer::i, j, k, sudokus(9,9,7)
+character(len=8) ::k_str
 !--------------------------------------------
-data (((sudokus(i,j,k),i=1,9),j=1,9),k=1,5) /&
+data (((sudokus(i,j,k),i=1,9),j=1,9),k=1,7) /&
 0,0,0,0,0,0,0,0,0,&
 0,0,0,0,0,0,0,0,0,&
 0,0,0,0,0,0,0,0,0,&
@@ -54,12 +55,31 @@ data (((sudokus(i,j,k),i=1,9),j=1,9),k=1,5) /&
 5,0,2,0,0,0,0,7,0,&
 0,0,0,0,6,0,0,3,0,&
 7,1,0,0,8,0,9,0,0,&
-0,0,0,9,0,0,0,0,0/  !the 299th sudoku in my first phone
+0,0,0,9,0,0,0,0,0,&  !the 299th sudoku in my first phone
+!--------------------
+0,0,0,0,0,6,0,0,0,&
+0,5,9,0,0,0,0,0,8,&
+2,0,0,0,0,8,0,0,0,&
+0,4,5,0,0,0,0,0,0,&
+0,0,3,0,0,0,0,0,0,&
+0,0,6,0,0,3,0,5,4,&
+0,0,0,3,2,5,0,0,6,&
+0,0,0,0,0,0,0,0,0,&
+0,0,0,0,0,0,0,0,0,& !http://norvig.com/sudoku.html hardest
+!--------------------
+0,0,0,0,0,5,0,8,0,&
+0,0,0,6,0,1,0,4,3,&
+0,0,0,0,0,0,0,0,0,&
+0,1,0,5,0,0,0,0,0,&
+0,0,0,1,0,6,0,0,0,&
+3,0,0,0,0,0,0,0,5,&
+5,3,0,0,0,0,0,6,1,&
+0,0,0,0,0,0,0,0,4,&
+0,0,0,0,0,0,0,0,0/ !http://norvig.com/sudoku.html unsolvable
 
 do k=3,4
 	call resolve(sudokus(:,:,k), 2)
 enddo
-
 end program main
 
 
@@ -70,21 +90,16 @@ logical::candidate(9,9,9), bug_flag, solved_flag
 !--------------------------------------------
 write(*,"(7X,9I2)") ((sudoku(i,j),i=1,9),j=1,9)
 write(*,*) "---"
-
+solved_flag=.false.
 select case(method)
-case(1) !computer's way
-	solved_flag=.false.
+case(1) !computer's way	
 	call try(1, sudoku, solved_flag)
-	if(solved_flag) then		
-		write(*,"(7X,9I2)") ((sudoku(i,j),i=1,9),j=1,9)
-		print*,"solved"
-	endif
 case(2) !human's way
 	call initialize(sudoku, candidate)
-	call solver(sudoku, candidate, .true.)
-	call check(sudoku, candidate, bug_flag, .true.)
+	call try_candidate(sudoku, candidate, solved_flag)	
 end select
 
+call check(sudoku, candidate, bug_flag, .true.)
 end subroutine resolve
 
 
@@ -133,15 +148,16 @@ do i=1,9
 	endif
 enddo
 enddo
+call solver(sudoku, candidate)
 end subroutine initialize
 
 
-subroutine solver(sudoku, candidate, recursive_flag)
+subroutine solver(sudoku, candidate)
 !https://www.sudokuwiki.org/Strategy_Families
 implicit none
-integer::sudoku(9,9), i, j, ij, guess, no_update_times
+integer::sudoku(9,9), no_update_times
 integer::n_sudoku, n_sudoku0, n_candidate, n_candidate0
-logical::candidate(9,9,9), recursive_flag, recursive_flag0
+logical::candidate(9,9,9)
 !--------------------------------------------
 no_update_times=0
 n_sudoku=count(sudoku>0)
@@ -154,22 +170,9 @@ do while(n_sudoku<81)
 	case(1)
 !		call process(2, sudoku, candidate) !Naked Pairs, Hidden Pairs; 
 !Naked Pairs, Hidden Pairs are actually included in Basic+Naked Triples, Hidden Triples
-		call process(3, sudoku, candidate) !Naked Triples, Hidden Triples
-	case(2:3) 
-		if (no_update_times==2) recursive_flag0=.false. !*-Wing ...
-		if (no_update_times==3) recursive_flag0=.true.
-		do ij=1,81
-			j=(ij-1)/9+1
-			i=mod(ij-1,9)+1
-			guess=count(candidate(:,i,j))
-			if((no_update_times==2 .and. guess .eq. 2) .or. &
-			   (no_update_times==3 .and. guess .ge. 2)) then
-				call try_candidate(i, j, sudoku, candidate, recursive_flag0)
-				if(count(sudoku>0) .ne. n_sudoku0) exit
-			endif	
-		enddo
-	case(4)
-		return
+!		call process(3, sudoku, candidate) !Naked Triples, Hidden Triples
+	case(2)
+		exit
 	end select
 !--------------------------------------------	
 	n_sudoku=count(sudoku>0)
@@ -177,7 +180,6 @@ do while(n_sudoku<81)
 	
 	if(n_sudoku0==n_sudoku .and. n_candidate0==n_candidate) then
 		no_update_times=no_update_times+1
-		if((.not. recursive_flag) .and. (no_update_times .eq. 2)) return
 	else 
 		n_sudoku0=n_sudoku
 		n_candidate0=n_candidate
@@ -201,7 +203,7 @@ do m=1,9
 	if(count(sudoku(k,:) .eq. m)>1 .or. & 
 	   count(sudoku(:,k) .eq. m)>1 .or. & 
 	   count(sudoku(i0:i0+2,j0:j0+2) .eq. m)>1) then
-		bug_flag=.true.
+		bug_flag=.true.		
 		exit0=.true.
 		exit   
 	endif
@@ -241,6 +243,7 @@ implicit none
 integer::sudoku(9,9), group(9), i_group, c9n, i_combination
 integer::i, j, k, n, m, way, i_series(9), j_series(9)
 logical::candidate(9,9,9), candidates(9), positions(9)
+integer::indgen(9)=(/1,2,3,4,5,6,7,8,9/)
 !--------------------------------------------
 call combination_number(9, n, c9n)
 do i_combination=1,c9n	
@@ -260,8 +263,8 @@ do way=1,3
 		enddo
 	endif
 !--------------------------------------------
-!if n candidates found in only n positions	
-	call ij_series2(k, way, i_series, j_series)
+!if n candidates found in only n positions
+	call ij_series(k, way, indgen, i_series, j_series)
 	forall(m=1:9) positions(m)=any(candidate(group(1:n), i_series(m), j_series(m)))
 	if (count(positions) .eq. n) &
 	forall(m=1:9, positions(m)) candidate(group(n+1:9), i_series(m), j_series(m))=.false.
@@ -292,22 +295,6 @@ case(3)
 	i_series=(mod(k-1,3))*3+1+mod(group-1,3)
 end select
 end subroutine ij_series
-
-
-subroutine ij_series2(k, way, i_series, j_series)
-integer::k, way, i_series(9), j_series(9)
-integer::indgen(9)=(/1,2,3,4,5,6,7,8,9/)
-!--------------------------------------------
-select case(way)
-case(1)
-	i_series=k; j_series=indgen
-case(2)
-	i_series=indgen; j_series=k
-case(3)
-	j_series=((k-1)/3)*3+1+(indgen-1)/3
-	i_series=(mod(k-1,3))*3+1+mod(indgen-1,3)
-end select
-end subroutine ij_series2
 
 
 subroutine combination_number(m, n, cmn)
@@ -342,7 +329,7 @@ enddo
 
 do k=n+1,9
 do j=1,k-1
-	if (all(j .ne. group(1:k-1)))  then 
+	if (all(j .ne. group(1:k-1))) then 
 		group(k)=j
 		exit
 	endif
@@ -381,29 +368,41 @@ enddo
 end subroutine findloc_all
 
 
-recursive subroutine try_candidate(i, j, sudoku, candidate, recursive_flag)
+recursive subroutine try_candidate(sudoku, candidate, solved_flag)
 implicit none
+logical::solved_flag
 integer::sudoku(9,9), sudoku_try(9,9), candidate_number(9), i, j, k
-logical::candidate(9,9,9), candidate_try(9,9,9), bug_flag, recursive_flag
+logical::candidate(9,9,9), candidate_try(9,9,9), bug_flag
 !--------------------------------------------
-call findloc_all(candidate(:,i,j), candidate_number) 
-do k=1, count(candidate(:,i,j))
-	sudoku_try=sudoku
-	candidate_try=candidate
-	sudoku_try(i,j)=candidate_number(k)
-	candidate_try(:,i,j)=.false.
-	candidate_try(candidate_number(k),i,j)=.true.
-	call solver(sudoku_try, candidate_try, recursive_flag)
-	call check(sudoku_try, candidate_try, bug_flag, .false.)
-	if (bug_flag) then 
-		candidate(candidate_number(k),i,j)=.false.
-		return
-	else
-		if(count(sudoku_try>0)==81)then
-			sudoku=sudoku_try
-			candidate=candidate_try
-			return	
-		endif		
-	endif
+solved_flag=count(sudoku>0)==81
+if (solved_flag) return
+
+do j=1,9
+do i=1,9
+if (count(candidate(:,i,j)) .ge. 2) then
+	call findloc_all(candidate(:,i,j), candidate_number)
+	do k=1, count(candidate(:,i,j))
+		sudoku_try=sudoku
+		sudoku_try(i,j)=candidate_number(k)
+		candidate_try=candidate
+		candidate_try(:,i,j)=.false.
+		candidate_try(candidate_number(k),i,j)=.true.
+		call solver(sudoku_try, candidate_try)
+		call check(sudoku_try, candidate_try, bug_flag, .false.)
+		if (bug_flag) then 
+			candidate(candidate_number(k),i,j)=.false.
+			call solver(sudoku, candidate)
+			call try_candidate(sudoku, candidate, solved_flag)
+			if (solved_flag) return
+		else
+			call try_candidate(sudoku_try, candidate_try, solved_flag)
+			if (solved_flag) then
+				sudoku=sudoku_try
+				return
+			endif
+		endif
+	enddo
+endif
+enddo
 enddo
 end subroutine try_candidate
